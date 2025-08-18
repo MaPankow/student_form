57d318b60877e0a7f94e951f7e01acafc84dfc09
MaPankow <162994783+MaPankow@users.noreply.github.com>
2025-08-18 17:25:34 +0200

    set up data structure and placeholders

---
+++ src/components/StudentForm/StudentForm.jsx
@@ -0,0 +1,11 @@
+function StudentForm() {
+    return (
+        <div>
+            <p>
+                Placeholder for student form
+            </p>
+        </div>
+    )
+};
+
+export default StudentForm;
