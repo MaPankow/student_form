b761b6579a0b40e756366ef82ae5dae469fd7623
MaPankow <162994783+MaPankow@users.noreply.github.com>
2025-09-24 13:06:29 +0200

    Import hook to App.jsx, create a button to toggle between light/dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.module.css';
 import StudentForm from './components/StudentForm/StudentForm';
 import styles from './App.module.css';
+import { useTheme } from './hooks/useTheme';
 
 function App() {
+
+  const { onToggleTheme, isDarkTheme } = useTheme();
 
 
   return (
     <div className={styles.app}>
       <h1 className={styles.h1}>Student Form App</h1>
       <div>
+        <button onClick={onToggleTheme}>Switch to light/dark mode</button>
         <StudentForm />
       </div>
 
